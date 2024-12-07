f451b6f99da5f9d6da5eb0088e472856b43371dd
omerspac <omerspac@gmail.com>
2024-12-07 15:38:54 +0500

    c5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,13 @@ import '@fortawesome/fontawesome-free/css/all.min.css'
 export default function Home() {
   return (
     <>
-      <p>test</p><br /><br /><br /><br /><br /><br /><br />
+      <div className="home-banner">
+        <p className="home-text">I am a</p>
+        <div className="home-animation-text">
+          <p className="home-text2">NextJS Developer</p>
+        </div>
+      </div>
+      <p>ez</p><br /><br /><br /><br /><br /><br /><br />
       <ScrollToTopButton />
     </>
   );
